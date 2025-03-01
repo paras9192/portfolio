0a34d4b3ee3675aab1beb515b04f047d328774b5
Paras Lohia <81807209+paras9192@users.noreply.github.com>
2025-03-02 01:06:08 +0530

    testing cors

--- chalkmate/settings/production.py
+++ chalkmate/settings/production.py
@@ -10,13 +10,8 @@ INSTALLED_APPS += THIRD_PARTY_APPS
 # added middlewares
 MIDDLEWARE = ["corsheaders.middleware.CorsMiddleware", *MIDDLEWARE]
 
-# CORS - Allow All
-# CORS_ALLOW_ALL_ORIGINS = True  # Allows all origins
-# # CORS_ALLOW_CREDENTIALS = True  # Allows cookies/auth headers
-# # CORS_ALLOW_HEADERS = ["*"]  # Allows all headers
-# # CORS_ALLOW_METHODS = ["*"]  # Allows all HTTP methods (GET, POST, etc.)
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 ACCESS_CONTROL_ALLOW_HEADERS=True
 CORS_ALLOW_HEADERS=[ '*']
 CORS_ALLOWED_ORIGIN_REGEXES = [
