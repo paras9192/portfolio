86ed3efdd3318deec77cd802e0fc1a84ea812f0c
Paras Lohia <81807209+paras9192@users.noreply.github.com>
2025-03-02 01:16:24 +0530

    testing cors

--- chalkmate/settings/production.py
+++ chalkmate/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["portfolio.yucampus.com", "localhost"]
+ALLOWED_HOSTS = ["portfolio.yucampus.com", "localhost","127.0.0.1"]
 
 # 3rd party apps
 THIRD_PARTY_APPS = ["corsheaders"]
