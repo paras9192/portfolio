a7600a56a37fe70634acd8fa6735ad9c4ffa8cfc
Paras Lohia <81807209+paras9192@users.noreply.github.com>
2025-07-28 20:19:57 +0530

    added allowed host

--- chalkmate/settings/production.py
+++ chalkmate/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["portfolio.yucampus.com", "localhost","127.0.0.1","portfolio-builder.yucampus.com"]
+ALLOWED_HOSTS = ["*"]
 
 # 3rd party apps
 THIRD_PARTY_APPS = ["corsheaders"]
